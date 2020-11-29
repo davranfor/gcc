44d82832b27654a24b0bfad09d4a2baad13f2e36
David Ranieri <50286622+davranfor@users.noreply.github.com>
2020-11-29 13:09:53 +0100

    Add files via upload

--- lambda.c
+++ lambda.c
@@ -6,7 +6,7 @@ int main(void)
 {
     __extension__
     double (*f)(double x[]) = lambda(double, (double x[]) { return x[0] * x[1]; });
-    double  x[] = {4.0, 5.0};
+    double x[] = {4.0, 5.0};
 
     printf("%f\n", f(x));
     return 0;
